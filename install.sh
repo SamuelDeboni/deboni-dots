fa88b31dca5d58cf4355ede74ca3bdf5ace2edbe
SamuelDeboni <samueldeboniss@gmail.com>
2019-06-14 19:22:14 -0300

    instaler update

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
 echo "Deboni Dots install script - TODO"
+sudo pacman -S calcurse pulseaudio volumeicon networkmanager-applet cbaticon comptom sxhkd
+nitrogen redshift
+
+cp .config ../
+cp .xinitrc ../
